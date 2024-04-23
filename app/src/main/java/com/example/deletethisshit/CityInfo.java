2b19acfd41d554d3920a2bc1adfd2ccd86f1c900
mashakrot <makrosensei@gmail.com>
2024-04-23 14:38:54 +0300

    list last searches

--- app/src/main/java/com/example/deletethisshit/CityInfo.java
+++ app/src/main/java/com/example/deletethisshit/CityInfo.java
@@ -1,38 +1,16 @@
 package com.example.deletethisshit;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.ActionBar;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
-
-import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class CityInfo extends AppCompatActivity {
     private String city;
-    private TextView txtPopulation;
-    private TextView txtWeather;
-    ArrayList<String> list = new ArrayList<>();
-
-
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,8 +24,6 @@ public class CityInfo extends AppCompatActivity {
         actionBar.setHomeAsUpIndicator(R.drawable.ic_back2);
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-
-//        ImageButton viewUsers = (ImageButton) findViewById(R.id.button2);
 
         TabPagerAdapter tabPagerAdapter = new TabPagerAdapter(this, city);
         viewPager.setAdapter(tabPagerAdapter);
@@ -81,17 +57,6 @@ public class CityInfo extends AppCompatActivity {
                 }
             }
         });
-
-
-//        ImageButton viewUsers = (ImageButton) findViewById(R.id.button2);
-//        viewUsers.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(CityInfo.this, MainActivity.class);
-//                finish();
-//                startActivity(intent);
-//            }
-//        });
     }
 
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
