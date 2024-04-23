a2198025d11f4a3b14f62f99f2c4dfd04d8c2cd8
mashakrot <makrosensei@gmail.com>
2024-04-23 14:25:15 +0300

    list last searches

--- app/src/main/java/com/example/deletethisshit/CityInfo.java
+++ app/src/main/java/com/example/deletethisshit/CityInfo.java
@@ -1,19 +1,28 @@
 package com.example.deletethisshit;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -21,6 +30,8 @@ public class CityInfo extends AppCompatActivity {
     private String city;
     private TextView txtPopulation;
     private TextView txtWeather;
+    ArrayList<String> list = new ArrayList<>();
+
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +39,15 @@ public class CityInfo extends AppCompatActivity {
         setContentView(R.layout.city_info);
         city = getIntent().getStringExtra("city");
 
-
         TabLayout tabLayout = findViewById(R.id.tabLayout);
         ViewPager2 viewPager = findViewById(R.id.viewArea);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_back2);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+//        ImageButton viewUsers = (ImageButton) findViewById(R.id.button2);
 
         TabPagerAdapter tabPagerAdapter = new TabPagerAdapter(this, city);
         viewPager.setAdapter(tabPagerAdapter);
@@ -66,17 +83,23 @@ public class CityInfo extends AppCompatActivity {
         });
 
 
-        ImageButton viewUsers = (ImageButton) findViewById(R.id.button2);
-        viewUsers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CityInfo.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+//        ImageButton viewUsers = (ImageButton) findViewById(R.id.button2);
+//        viewUsers.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(CityInfo.this, MainActivity.class);
+//                finish();
+//                startActivity(intent);
+//            }
+//        });
     }
 
-/*    public void onSearchButtonClick(View view) {
-
-    }*/
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
